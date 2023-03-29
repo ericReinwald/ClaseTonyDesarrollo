ba2e819ff1481ce092d8563f156d216894e0b1bf
Eric reinwald <ericreind778@gmail.com>
2023-03-29 16:03:11 -0300

    Books+Api

---
+++ src/components/books/BookItem.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+import BookCard from "../ui/BookCard";
+
+import "./BookItem.css";
+import ReadDate from "../books/ReadDate";
+
+const Bookitem = ({
+  author,
+  title,
+  dateRead,
+  pageCount,
+  readingProgress,
+  rating,
+}) => {
+  const [newTitle, setNewTitle] = useState("");
+
+  const clickHandler = () => {
+    console.log("click");
+    setNewTitle("Leido!");
+  };
+
+  return (
+    <BookCard className="book-item-container">
+      <h2>{title}</h2>
+      {<h2>{newTitle}</h2>}
+      <h3> Autor: {author} </h3>
+      <ReadDate dateRead={dateRead} />
+      <p> {pageCount} Paginas</p>
+
+      <div>
+        <button onClick={clickHandler}>Marcar como Leido </button>
+      </div>
+      <div>
+        {readingProgress ? (
+          <div>
+            <p> El progreso de lectura es : {readingProgress + "%"}</p>
+          </div>
+        ) : (
+          <p>No hay progreso de lectura</p>
+        )}
+
+        {rating ? (
+          <div>
+            <p> las estrellas son: {rating}</p>
+          </div>
+        ) : undefined}
+      </div>
+    </BookCard>
+  );
+};
+
+export default Bookitem;
---
+++ src/components/books/Books.js
@@ -0,0 +1,55 @@
+import "./Books.css";
+import { useState } from "react";
+import BooksFilter from "./filter/BooksFilter";
+import Bookitem from "./BookItem";
+
+const Books = ({ books }) => {
+  const [YearFilter, setyearFilter] = useState("all");
+
+  const onYearFilterChanged = (value) => {
+    setyearFilter(value);
+  };
+
+  const booksMapped =
+    YearFilter === "all"
+      ? books.map((book) => (
+          <Bookitem
+            key={book.id}
+            title={book.title}
+            dateRead={book.dateRead}
+            author={book.author}
+            pageCount={book.pageCount}
+            readingProgress={book.readingProgress}
+            rating={book.rating}
+          />
+        ))
+      : books
+          .filter(
+            (books) => books.dateRead.getFullYear().toString() === YearFilter
+          )
+          .map((book) => (
+            <Bookitem
+              key={book.id}
+              title={book.title}
+              dateRead={book.dateRead}
+              author={book.author}
+              pageCount={book.pageCount}
+              readingProgress={book.readingProgress}
+              rating={book.rating}
+            />
+          ));
+
+  return (
+    <div>
+      <BooksFilter onYearChanged={onYearFilterChanged} year={YearFilter} />
+      <div className="books-container">
+        {booksMapped.length === 0 ? (
+          <p>No leiste los libros en el {YearFilter} </p>
+        ) : (
+          booksMapped
+        )}
+      </div>
+    </div>
+  );
+};
+export default Books;
